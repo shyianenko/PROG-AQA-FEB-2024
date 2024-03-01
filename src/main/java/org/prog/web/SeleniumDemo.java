87869be80eb753d268bfae6636077054eae2aa11
shyianenko <shyianenko@gmail.com>
2024-03-01 13:23:25 +0200

    Home work Session 6 Thread.sleep were added and commented. Had issue with errors on searchInput.sendKeys(Keys.ENTER) (35)

--- src/main/java/org/prog/web/SeleniumDemo.java
+++ src/main/java/org/prog/web/SeleniumDemo.java
@@ -17,50 +17,32 @@ import java.util.List;
 
 public class SeleniumDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         try {
             driver.get("https://rozetka.com.ua");
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10L));
             //accept cookies if they are present
             //find all buttons in cookies form
-//            List<WebElement> cookieButtons =
-//                    driver.findElements(By.xpath(
-//                            "//a[contains(@href,'https://policies.google.com/technologies/cookies')]/../../../..//button"));
-//            if (!cookieButtons.isEmpty()) {
-//                //click button with index 3 (this button accepts all cookies)
-//                cookieButtons.get(3).click();
-//            }
+
             //TODO: sreach for razer
             //Step 1: set search form value to razer
             WebElement searchInput = driver.findElement(By.name("search"));
             WebElement searchButton = driver.findElement(By.xpath("//button[contains(@class, 'search-form__submit')]"));
             searchInput.sendKeys("razer blackshark v2 pro");
+//            Thread.sleep(4000);
             //Step 2: press enter while in search form
-            //searchInput.sendKeys(Keys.ENTER);
-            wait.until(ExpectedConditions.visibilityOf(searchButton));
-            searchButton.click();
+            searchInput.sendKeys(Keys.ENTER);
+//            wait.until(ExpectedConditions.visibilityOf(searchButton));
+//            searchButton.click();
 
             List<WebElement> searchGoods = driver.findElements(By.xpath("//div[@class='goods-tile__inner']//span[@class='goods-tile__title']"));
-
+//            Thread.sleep(4000);
             wait.until(ExpectedConditions.visibilityOfAllElements(searchGoods));
             WebElement firstGood = searchGoods.get(0);
             String titleOfFirstGood = firstGood.getText();
             System.out.println(titleOfFirstGood);
 
-//            System.out.println(">>>>> " + searchItems.size());
-//
-//            int razerFound = 0;
-//            for (WebElement element : searchItems) {
-//                if (element.getText().contains("razer blackshark v2 pro")) {
-//                    razerFound++;
-//                }
-//            }
-//            if (razerFound >= 3) {
-//                System.out.println("Ben found!");
-//            } else {
-//                System.out.println("Ben not found :(");
-//            }
         } finally {
             driver.quit();
         }
