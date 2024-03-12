a444404f9f860c957cfae5320d987e6a86040d55
shyianenko <shyianenko@gmail.com>
2024-03-12 15:09:24 +0200

    Home work Session 9 idDto were added show randomuser id's with name and value

--- src/test/java/org/prog/testng/RestTests.java
+++ src/test/java/org/prog/testng/RestTests.java
@@ -9,6 +9,9 @@ import org.prog.dto.ResultsDto;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
 
 public class RestTests {
@@ -18,23 +21,15 @@ public class RestTests {
     // add new Dto to existing PersonDto class
     // print "name" and "value" from IdDto
 
+    List<PersonDto> personDtoList = new ArrayList<>();
+
     @Test
     public void getRandomPersonTest() {
         RestAssured.baseURI = "https://randomuser.me/";
-//        Response response =
-//                RestAssured.get("api/?inc=gender,name,nat&noinfo");
-//
-//        response.prettyPrint();
-//
-//        ValidatableResponse vr = response.then();
-//
-//        vr.statusCode(200);
-//        vr.contentType(ContentType.JSON);
-
         RequestSpecification rs = RestAssured.given()
                 .header("X-Client", "prog qutomation")
                 .body("reqeust body")
-                .queryParam("inc", "gender,name,nat")
+                .queryParam("inc", "gender,name,nat,id")
                 .queryParam("noinfo")
                 .queryParam("results", 10)
                 .basePath("api/");
@@ -60,5 +55,29 @@ public class RestTests {
             }
         }
         Assert.assertTrue(anyFemalePresent, "No males found");
+        System.out.println("-------------------------");
+
+        personDtoList = dto.getResults();
+    }
+
+    @Test
+    public void getRandomId() {
+        RestAssured.baseURI = "https://randomuser.me/";
+        RequestSpecification rs = RestAssured.given()
+                .header("X-Client", "prog qutomation")
+                .body("reqeust body")
+                .queryParam("inc", "gender,name,nat,id")
+                .queryParam("noinfo")
+                .queryParam("results", 10)
+                .basePath("api/");
+        rs.auth().basic("user", "password");
+
+        Response response = rs.get();
+
+        ResultsDto dto = response.as(ResultsDto.class);
+        for (PersonDto p : dto.getResults()) {
+            System.out.println(p.getId().getName() + " " + p.getId().getValue());
+
+        }
     }
 }
